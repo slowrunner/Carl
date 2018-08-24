1b622c6ce636438327ac204175b2ad2cf2996d46
slowrunner <slowrunner@users.noreply.gethub.com>
2018-08-24 15:25:39 -0400

    first performance test

---
+++ systests/performance/eMotor_Perf.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# eMotors_Perf.py
+#
+# Measure performance of some motor API calls
+# Method:  Use time.perf_counter() and time.process_time() to measure asynchronous motor commands
+
+# force Python3 compatibility for print, integer division, and input()
+from __future__ import print_function
+from __future__ import division
+from builtins import input
+
+# import the time library for sleep(), perf_counter() and process_time()
+import time
+
+# import the EasyGoPiGo3 module
+import easygopigo3 as easy
+import sys
+import atexit
+
+# Create an instance egpg of the EasyGoPiGo3 class.
+egpg = easy.EasyGoPiGo3()
+atexit.register(egpg.stop)   #call egpg.stop() before exiting program
+egpg.reset_all()
+time.sleep(1)  # for reset to finish
+
+
+print("About to command motors fwd")
+time.sleep(1)
+start_pc=time.perf_counter()
+egpg.forward()
+end_pc=time.perf_counter()
+print("EasyGoPiGo3.forward() perf_counter time: %f ms \n" % (end_pc - start_pc)*1000 )
+
+print("Stopping the motors after 1 second.")
+time.sleep(1)
+start_pc=time.perf_counter()
+gpg.stop()
+end_pc=time.perf_counter()
+print("EasyGoPiGo3.stop() perf_counter time: %f ms \n" % (end_pc - start_pc)*1000 )
+
+print("About to command motors bwd")
+time.sleep(1)
+start_pc=time.perf_counter()
+egpg.backward()
+end_pc=time.perf_counter()
+print("EasyGoPiGo3.backward() perf_counter time: %f ms \n" % (end_pc - start_pc)*1000 )
+
+print("Stopping the motors after 1 second.")
+time.sleep(1)
+start_pc=time.perf_counter()
+gpg.stop()
+end_pc=time.perf_counter()
+print("EasyGoPiGo3.stop() perf_counter time: %f ms \n" % (end_pc - start_pc)*1000 )
+
+print("EasyGoPiGo3.reset_all()")
+egpg.reset_all()
+time.sleep(1)  # allow time for reset to complete
+print("Done!")
