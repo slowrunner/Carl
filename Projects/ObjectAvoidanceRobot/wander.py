037a81a6677c8dec19b825f4258766d46f0cbc9b
slowrunner <slowrunner@users.noreply.github.com>
2018-09-04 16:08:57 -0400

    added use_mutex=True to EasyGoPiGo3 instantiation, removed reset_all() everywhere

--- Projects/ObjectAvoidanceRobot/wander.py
+++ Projects/ObjectAvoidanceRobot/wander.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+#  wander.py        Move forward at MAX_SPEED, slowing then stopping MIN_DISTANCE from an obstacle
+#                   After "thinking", execute TURN_TO_AVOID (135 deg right) and resume loop
+#
+#                   Based on DI Projects/ObjectAvoidanceRobot
+#                   with addition of ovoid_obstacle(), check_battery_voltage(), use_mutex=True
+#                                    copious print statments, slowing only after MAX_DISTANCE
+#                                    handle distance_sensor.enableDebug() not defined error
+#                                    added more status at exit
+#
+#                   Assumes Distance Sensor points forward, with no servos,
+#                           so if servo(s) exist: center servo(s) before running this program
+#
+
 """
 GoPiGo3 for the Raspberry Pi: an open source robotics platform for the Raspberry Pi.
 Copyright (C) 2017  Dexter Industries
@@ -25,12 +38,13 @@ import signal
 from time import sleep
 
 DEBUG = False # if set to True, any exception that's encountered is debugged
-MAX_DISTANCE = 2300 # measured in mm
+MAX_DISTANCE = 900  # was 2300 # measured in mm - Alan: Distance to start slowing down
 MIN_DISTANCE = 225  # measured in mm (was 150)
 NO_OBSTACLE = 3000
 ERROR = 0 # the error that's returned when the DistanceSensor is not found
 MAX_SPEED = 300 # max speed of the GoPiGo3
 MIN_SPEED = 100 # min speed of the GoPiGo3
+TURN_TO_AVOID=135
 
 # variable for triggering the closing procedure of the script
 # used for stopping the while loop that's in the Main() function
@@ -48,11 +62,12 @@ def debug(string):
     if DEBUG is True:
         print("Debug: " + str(string))
 
-# added for sd card safety
+# added for sd card safety - Alan McDonley
 batteryLowCount = 0
-def check_battery_voltage(LOW_BATTERY_V=7.5):
+LOW_BATTERY_VOLTAGE = 8.5  # Stop at about 20% charge left for long battery life
+def check_battery_voltage(LOW_BATTERY_V=LOW_BATTERY_VOLTAGE):
         global gopigo3
-        vBatt = gopigo3.get_voltage_battery()
+        vBatt = gopigo3.volt()  # use the thread-safe version
         if (vBatt < LOW_BATTERY_V): 
             batteryLowCount += 1
         else: batteryLowCount = 0
@@ -63,8 +78,21 @@ def check_battery_voltage(LOW_BATTERY_V=7.5):
           time.sleep(1)
         return 0
 
+# added to wander after stopping near an obstacle
+def avoid_obstacle():
+    global gopigo3, determined_speed
+
+    determined_speed = MAX_SPEED
+    gopigo3.set_speed(determined_speed)
+    print("Maybe I should try spinning?")
+    gopigo3.turn_degrees(TURN_TO_AVOID)  # turn to avoid and then continue
+    print("Waiting for the world to stop spinning.\n\n")
+    sleep(15)
+
+############## MAIN ##############
+
 def Main():
-    global gopigo3
+    global gopigo3, determined_speed
 
     print("   _____       _____ _  _____         ____  ")
     print("  / ____|     |  __ (_)/ ____|       |___ \ ")
@@ -77,7 +105,7 @@ def Main():
     # initializing an EasyGoPiGo3 object and a DistanceSensor object
     # used for interfacing with the GoPiGo3 and with the distance sensor
     try:
-        gopigo3 = EasyGoPiGo3()
+        gopigo3 = EasyGoPiGo3(use_mutex=True)   # added use_mutex=True to allow status in another shell
         distance_sensor = gopigo3.init_distance_sensor()
 
     except IOError as msg:
@@ -96,7 +124,8 @@ def Main():
         sys.exit(1)
 
     if DEBUG is True:
-        distance_sensor.enableDebug()
+        # distance_sensor.enableDebug()
+        print("distance_sensor has no enableDebug() method")
 
     # variable that says whether the GoPiGo3 moves or is stationary
     # used during the runtime
@@ -106,8 +135,9 @@ def Main():
 
     # while the script is running
     while robot_operating:
-        # protect from running battery down
+        # protect from running battery down  - added Alan McDonley
         check_battery_voltage()
+
         # read the distance from the distance sensor
         current_distance = distance_sensor.read_mm()
         determined_speed = 0
@@ -123,13 +153,10 @@ def Main():
             gopigo3.stop()
             gopigo3_stationary = True
             print("Current distance : {:4} mm Current speed: {:4} Stopped: {}".format(current_distance, int(determined_speed), gopigo3_stationary is True ))
-            print("Blocked!  Thinking about what to do")
+            # added to continue after reaching a blocking obstacle - Alan McDonley
+            print("\n\n*** Blocked!  Thinking about what to do ***")
             sleep(15)
-            determined_speed = MAX_SPEED
-            gopigo3.set_speed(determined_speed)
-            gopigo3.turn_degrees(-135)  # turn 135 degrees and continue
-            print("Waiting for the world to stop spinning.")
-            sleep(15)
+            avoid_obstacle()
         # if the robot is far away from the target
         else:
             gopigo3_stationary = False
@@ -138,13 +165,18 @@ def Main():
             if current_distance == NO_OBSTACLE:
                 # then set the speed to maximum
                 determined_speed = MAX_SPEED
+                print("\nNothing to stop me now")
+            # if the robot is farther than MAX_DISTANCE  - Alan McDonley
+            elif current_distance > MAX_DISTANCE:
+                # then set the speed to maximum
+                determined_speed = MAX_SPEED
+                print("\nMight be something up ahead")
             else:
                 # otherwise, calculate the speed with respect to the distance from the target
                 percent_speed = float(current_distance - MIN_DISTANCE) / (MAX_DISTANCE - MIN_DISTANCE)
                 determined_speed = MIN_SPEED + (MAX_SPEED - MIN_SPEED) * percent_speed
-
+                print("\n!!!! PROCEEDING WITH CAUTION !!!!")
             # apply the changes
-            print("LET'S GO!")
             gopigo3.set_speed(determined_speed)
             gopigo3.forward()
 
@@ -158,9 +190,9 @@ def Main():
 
     # and finally stop the GoPiGo3 from moving
     gopigo3.stop()
-    print("Exiting")
-    print("Battery Voltage: %.1f" % gopigo3.volt())
-    print("Current distance : {:4} mm Stopped: {}".format(current_distance, gopigo3_stationary is True ))
+    print("\n***** Exiting *****")
+    print("Battery Voltage: %.1f   Robot Operating: %s" % (gopigo3.volt(), robot_operating))
+    print("Current distance : {:4} mm Was Stopped: {}".format(current_distance, gopigo3_stationary is True))
 
 
 if __name__ == "__main__":
