8e5bf782e02e83c23c422aadf79f9ee45f45a10e
slowrunner <slowrunner@users.noreply.gethub.com>
2018-09-05 15:54:42 -0400

    in progress

--- Projects/ServoScan/ds_servo_scan.py
+++ Projects/ServoScan/ds_servo_scan.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-############################################################################################                                                                
+############################################################################################
 # This example creates LIDAR like map using an ultrasonic sensor and a servo with the GoPiGo
-#                                
-# http://www.dexterindustries.com/GoPiGo/                                                                
+#
+# http://www.dexterindustries.com/GoPiGo/
 # History
 # ------------------------------------------------
 # Author     	Date      		Comments
 # McDonley               5 Sept 18      Update to EasyGoPiGo3 with TOF Distance Sensor
-# Karan		  	13 June 14  	Initial Authoring           
-#                                       
+# Karan		  	13 June 14  	Initial Authoring
+#
 '''
 ## License
- GoPiGo for the Raspberry Pi: an open source robotics platform for the Raspberry Pi.
- Copyright (C) 2017  Dexter Industries
+ GoPiGo3 for the Raspberry Pi: an open source robotics platform for the Raspberry Pi.
+ Copyright (C) 2018  Dexter Industries
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -26,21 +26,25 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
-'''      
+'''
 #
 ############################################################################################
 #
 # ! Attach DI Distance Sensor (VL53L0X based) to GoPiGo3 SERVO1 Port.
 #
 ############################################################################################
-import easygopigo3
-import sys
-from collections import Counter
-import math
 
 #
 from __future__ import print_function
 from __future__ import division
+#
+import easygopigo3
+import sys
+from collections import Counter
+import math
+from time import sleep
+
+debug = True			# True to print all raw values
 
 
 # Create an instance egpg of the GoPiGo3 class.
@@ -52,24 +56,25 @@ egpg = easygopigo3.EasyGoPiGo3(use_mutex=True)    # use_mutex=True for "thread-s
 # The EasyDistanceSensor will return trusted readings out to roughly 230 cm and returns 300 when no obstacle seen
 
 distance_sensor = egpg.init_distance_sensor()
+servo = egpg.init_servo("SERVO1")
 
-PERSONAL_SPACE=25			# Stop when nearest object is within this distance in cm
+REVERSE_AXIS=True		# Need to reverse axis if servo is mounted shaft down (tilt/pan assembly)
+PERSONAL_SPACE=25		# Stop when nearest object is within this distance in cm
+num_of_readings=45		# Number of trusted readings to use
+incr=180/num_of_readings	# increment of angle in servo
+ang_l=[0]*(num_of_readings+1)	# list to hold the angle of each trusted readings
+dist_l=[0]*(num_of_readings+1)	# list to hold the distance (trusted_reading) at each angle
+x=[0]*(num_of_readings+1)	# list to hold the x coordinate of each point
+y=[0]*(num_of_readings+1)	# list to hold the y coordinate of each point
+lim=230				# trust limit of distance sensor in cm (any sample over this which be set to the limit value)
 
 #
 # Scan for map data
 #
 def ds_map():
 	delay=.02
-	debug = True			# True to print all raw values
-	num_of_readings=45		# Number of trusted readings to use 
-	incr=180/num_of_readings	# increment of angle in servo
-	ang_l=[0]*(num_of_readings+1)	# list to hold the angle of each trusted readings
-	dist_l=[0]*(num_of_readings+1)	# list to hold the distance (trusted_reading) at each angle
-	x=[0]*(num_of_readings+1)	# list to hold the x coordinate of each point
-	y=[0]*(num_of_readings+1)	# list to hold the y coordinate of each point
 
 	ang=0
-	lim=230			# trust limit of distance sensor in cm (any sample over this which be set to the limit value)
 	index=0
 	samples=5		# Number of samples for each angle (more samples yields more trust, but takes more time)
 	buf=[0]*samples		# buffer to hold sample readings at one angle
@@ -77,19 +82,22 @@ def ds_map():
 
 
 
-	print "*** SCANNING ***"
-	
+	print("*** SCANNING ***")
+
 	while True:
-		#Take the readings from the Ultrasonic sensor and process them to get the correct values
+		if debug:
+		    print("\nAngle: {} deg".format(ang))
+
+		#Take the readings from the Distance sensor for this angle, validate within limit
 		for i in range(samples):
-			dist=ds.read_cm()
+			dist=distance_sensor.read()  # in cm
 			if dist<lim and dist>=0:
 				buf[i]=dist
 			else:
 				buf[i]=lim
-		
-		#Find the sample that is most common among all the samples for a particular angle
-		max=Counter(buf).most_common()	
+
+		#Find the sample that is most common among all the samples for this angle
+		max=Counter(buf).most_common()
 		trusted_reading=-1
 		for i in range (len(max)):
 			if max[i][0] <> lim and max[i][0] <> 0:
@@ -97,20 +105,21 @@ def ds_map():
 				break
 		if trusted_reading==-1:
 			trusted_reading=lim
-		
-		if debug==1:
-                        print("Distance Sensor:{} deg {} mm ".format(index,ang,trusted_reading))
+
+		if debug:
+                        print("Index:{} Angle:{} deg Distance:{} mm ".format(index,ang,trusted_reading))
 			# print index,ang,trusted_reading
 		ang_l[index]=ang
 		dist_l[index]=trusted_reading
 		index+=1
 
 		#Move the servo to the next angle
-		servo(ang)	
-		time.sleep(delay)
+		if not REVERSE_AXIS:
+			servo.rotate_servo(ang)		# DI Servo Package has shaft up, 0 = left
+		else:
+			servo.rotate_servo(180-ang)	# Shaft down servo configuration 0 = right
+		sleep(delay)
 		ang+=incr
-		if debug:
-		    print("Angle: {} deg".format(ang))
 		if ang>180:
 			break
 
@@ -119,41 +128,50 @@ def ds_map():
 #
 # Print map data
 #
-def print_map():	
+def print_map():
 	#Print the values in a grid of 51x51 on the terminal
 	grid_size=51
-	
+
 	#Convert the distance and angle to (x,y) coordinates and scale it down
-	for i in range(num_of_readings+1):	
+	if debug:  print("Scaled Cartesian Data")
+	for i in range(num_of_readings+1):
 		x[i]=(int(dist_l[i]*math.cos(math.pi*(ang_l[i])/180))/10)
 		y[i]=int(dist_l[i]*math.sin(math.pi*ang_l[i]/180))/10
-	
+		if debug:
+		   print("x[{}] y[{}]=[{} {}]".format(i,i,x[i],y[i]))
+
 	#Rotate the readings so that it is printed in the correct manner
-	for i in range(num_of_readings+1):	
+	if debug:  print("Rotated for printing")
+	for i in range(num_of_readings+1):
 		x[i]=(grid_size/2)-x[i]
 		y[i]=(grid_size/2)-y[i]
+		if debug:
+		   print("x[{}] y[{}]=[{} {}]".format(i,i,x[i],y[i]))
 
 	#Create a grid
-	grid = [[0 for a in xrange(grid_size)] for a in xrange(grid_size)] 
+	grid = [[0 for a in xrange(grid_size)] for a in xrange(grid_size)]
+	if debug:  print("Put value 1 in grid for each trusted_reading")
 	for i in range (num_of_readings+1):
 		if dist_l[i]<>lim:
-			grid[y[i]][x[i]]=1	
+			if debug:
+			    print("y[{}]:{} x[{}]:{}".format(i,y[i],i,x[i]))
+			grid[int(y[i])][int(x[i])]=1
 	fence='-'*(grid_size+1)
-	
+
 	#Print the map
-	print "Map:"
-	print fence*2
-	for i in range(grid_size/2):
-		print "|",
+	print("Map:")
+	print(fence*2)
+	for i in range(int(grid_size/2)):
+		print("|"),
 		for j in range (grid_size):
-			if (j==grid_size/2)and i==(grid_size/2)-1:
-				print 'x',
+			if (j==int(grid_size/2)) and i==(int(grid_size/2)-1):
+				print("+"),
 			elif grid[i][j]==0:
-				print ' ',
+				print(" "),
 			else:
-				print 'o',
-		print "|"
-	print fence*2
+				print("o"),
+		print("|")
+	print(fence*2)
 	closest_obj=min(dist_l)
         print("Closest Object: {} cm".format(closest_obj))
 	return closest_obj	#Return the closest distance in all directions
@@ -161,7 +179,7 @@ def print_map():
 
 
 # MAIN
-try:	
+try:
 	egpg.stop()
 	while True:
 		#Make Map, GoPiGo move forward if no object within , stops makes a map and again moves forward
@@ -174,8 +192,10 @@ try:
 		print("*** WE HAVE CLEARANCE TO MOVE ***") 
 		egpg.drive_cm(closest_object * 0.3)	# drive 1/3 of the distance to closest object
 
-		print("*** STOPPING TO ENJOY THE VIEW ***")		
-		time.sleep(5)
+		print("*** STOPPING TO ENJOY THE VIEW ***")
+		sleep(5)
+	servo.rotate_servo(90)
 
 except KeyboardInterrupt:
 	print("**** Ctrl-C detected.  Finishing Up ****")
+	servo.rotate_servo(90)
