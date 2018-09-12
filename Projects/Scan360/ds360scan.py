fe33104b2a3a08cf7c2d8d5779b1612f63dfc13f
slowrunner <slowrunner@users.noreply.gethub.com>
2018-09-12 16:58:56 -0400

    working

--- Projects/Scan360/ds360scan.py
+++ Projects/Scan360/ds360scan.py
@@ -15,7 +15,7 @@
 from __future__ import print_function # use python 3 syntax but make it compatible with python 2
 from __future__ import division       #                           ''
 
-from time import sleep
+from time import sleep, clock
 import easygopigo3 # import the GoPiGo3 class
 import math
 import printmaps
@@ -29,7 +29,7 @@ distance_sensor = egpg.init_distance_sensor()
 
 # GOPIGO3 CONSTANTS
 
-TICS_PER_WHEEL_DEGREE = 2.0
+
 WHEEL_DIAMETER = egpg.WHEEL_DIAMETER                    # 66.5 in mm   (measures 65.89 from circumfirence/pi)
 WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * math.pi          # 208.92 mm    (measures 207 )
 WHEEL_BASE_WIDTH = egpg.WHEEL_BASE_WIDTH                # 117 in mm    (static 114-115, cannot measure dynamic)
@@ -41,10 +41,29 @@ def motors_running():
     motors_state = egpg.get_motor_status(egpg.MOTOR_LEFT)[3] | egpg.get_motor_status(egpg.MOTOR_RIGHT)[3]
     return (motors_state != 0)
 
+#
+#   ENCODER AVERAGE TO SPIN DEGREES
+def encoder_ave_to_spin_deg(encoder_ave):
+
+    # deg = (WHEEL_DIAMETER*pi * encoder_ave/360)   / (WHEEL_BASE_WIDTH*pi /360) )
+    # simplify by *pi/*pi  360/360
+    d = WHEEL_DIAMETER * encoder_ave / WHEEL_BASE_WIDTH
+    return d
+
+#
+#   SPIN AND SCAN 
+#	Spin in place taking distance sensor readings as often as possible
+#	At the default speed (100) takes about 36 readings on a RPi 3B (four-core 1.2GHz)
+#	At speed=300 takes about 15 readings
+#
 def spin_and_scan(degrees=360, speed=100):
-    debug = True
+    debug = False
+    timing= False
+
     reading_l = []
     at_angle_l = []
+    ave_enc_l = []
+    readings = 0
 
     # reset encoders to 0
     egpg.reset_encoders()
@@ -53,45 +72,54 @@ def spin_and_scan(degrees=360, speed=100):
     egpg.set_speed(speed)
 
     # Take and Store Reading
-    reading_l += [distance_sensor.read()]    # in cm
-    at_angle_l += [90.0]                     # First reading straight ahead
+    reading_l += [distance_sensor.read_mm()]     # in mm to keep precision
+    left_enc,right_enc = egpg.read_encoders() # degrees each wheel turned (should be 0)
+    ave_enc_l +=[ (abs(left_enc) + abs(right_enc)) / 2.0]   # save ave encoder for first reading
+    if debug:
+	print("left enc:{} right enc:{}  ave enc:{:.1f}".format(left_enc,right_enc,ave_enc_l[readings]))
     readings = 1
 
 
-    # calculate an initial delay to limit readings loop
-    time_360_at_100 = 4.0 # seconds
-    default_readings=16
-    reading_delay = time_360_at_100/default_readings * 0.9
+    # calculate an initial delay to let motion begin before next reading
+    startup_delay = 0.3 * 100/speed  # seconds
 
     # Start the spin
     egpg.turn_degrees(degrees,blocking=False)
 
     # delay before reading again
-    sleep(reading_delay)
+    sleep(startup_delay)
+
+    # start a timer
+    if timing: start = clock()
 
     # probably should have a timeout safety exit on this loop
     while motors_running():
-        # take a new reading
-        reading_l += [distance_sensor.read_mm()]    #in mm for no loss of precision
 
         # Find out how much we've spun so far
         left_enc,right_enc = egpg.read_encoders()      		# degrees each wheel turned
-        ave_encoders = (abs(left_enc) + abs(right_enc)) / 2.0   # in case one turns a little faster
-	if debug:
-	    print("left enc:{} right enc:{}  ave enc:{:.1f}".format(left_enc,right_enc,ave_encoders))
+        ave_enc_l +=[ (abs(left_enc) + abs(right_enc)) / 2.0]   # save average in case one turns a little faster
 
-        # starting at GoPiGo3 angle 90  (0 = left  90 = straight ahead)
-        # angle = 90 + (Wheel Dia * Ave_Enc) / Wheel Base Cir     # simplified by *pi/pi) and *360/360 cancels
-        at_angle_l +=  [( (WHEEL_DIAMETER * ave_encoders) / WHEEL_BASE_CIRCUMFERENCE) % 360 ]
+        # take a new reading
+        reading_l += [distance_sensor.read_mm()]    #in mm for no loss of precision
+
+	if debug:
+	    print("\nleft enc:{} right enc:{} ave enc:{:.1f}".format(left_enc,right_enc,ave_enc_l[readings]))
+	    print("Readings:{} Reading:{:.1f} spun:{:.1f} deg".format(readings, \
+								reading_l[readings], \
+								encoder_ave_to_spin_deg(ave_enc_l[readings])))
+	# increment readings count for this datum
         readings += 1
 
-	if debug:
-	   print("Readings:{} Reading:{:.1f} Angle:{:.1f}".format(readings, \
-								reading_l[readings-1], at_angle_l[readings-1]))
+	# timer to measure loop
+        #if timing: reading_took=clock() - start
 
-	# adjust delay to get requested number of readings during the spin
-        sleep(reading_delay)
+        #sleep(reading_delay)
+	#if timing: start = clock()
 
+    # convert average encoder reading to degrees turned and add 90 degrees for start angle
+    # angle = 90 + (Wheel Dia * Ave_Enc) / Wheel Base Cir     # simplified by *pi/pi) and *360/360 cancels
+    at_angle_l =  [( (90 + encoder_ave_to_spin_deg(ave_encoders))  % 360) for ave_encoders in ave_enc_l  ]
+    if timing: print("last reading took:",reading_took)
     return reading_l, at_angle_l
 
 
@@ -103,7 +131,18 @@ def main():
     at_angle_list = []
 
     try:
+	print("\nSPIN 360 AND SCAN at speed=100")
         dist_list_mm,at_angle_list = spin_and_scan(360, speed=100)   # spin in place and take distance sensor readings
+        range_list_cm = [ dist/10 for dist in dist_list_mm ]
+        printmaps.view360(range_list_cm, at_angle_list)                # print view (all r positive, theta 0=left)
+
+	print("\nSPIN 360 AND SCAN at speed=300")
+        dist_list_mm,at_angle_list = spin_and_scan(360, speed=300)   # spin in place and take distance sensor readings
+        range_list_cm = [ dist/10 for dist in dist_list_mm ]
+        printmaps.view360(range_list_cm, at_angle_list)                # print view (all r positive, theta 0=left)
+
+	print("\nSPIN 360 AND SCAN at speed=50")
+        dist_list_mm,at_angle_list = spin_and_scan(360, speed=50)   # spin in place and take distance sensor readings
         range_list_cm = [ dist/10 for dist in dist_list_mm ]
         printmaps.view360(range_list_cm, at_angle_list)                # print view (all r positive, theta 0=left)
         egpg.stop()
