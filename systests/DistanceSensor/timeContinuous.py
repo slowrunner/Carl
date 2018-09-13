2c31628fb9528d686a91abeedd1f0b48459eb926
slowrunner <slowrunner@users.noreply.gethub.com>
2018-09-13 11:35:18 -0400

    working

--- systests/DistanceSensor/timeContinuous.py
+++ systests/DistanceSensor/timeContinuous.py
@@ -8,15 +8,18 @@ from __future__ import division
 from di_sensors.distance_sensor import DistanceSensor
 import time
 
-print("\nExample program for timing a single read of the Dexter Industries Distance Sensor.")
+print("\nExample program for timing continuous mode reads of the Dexter Industries Distance Sensor.")
 
 ds = DistanceSensor()
+ds.start_continuous()
 
 for i in range(10):
   # read the distance as a single-shot sample
   start=time.clock()
-  read_distance = ds.read_range_single()
+  read_distance = ds.read_range_continuous()
   timing=time.clock() - start
   print("Distance from object: {} mm".format(read_distance))
   print("Timing: {:.3f}".format(timing))
-  time.sleep(1)
+  # delay for 1/2 single read timing to ensure a new reading each time
+  # (repeat test, increasing delay from 0.001 until no repeats seen in timings)
+  time.sleep(0.005)
--- systests/DistanceSensor/timeEasyDS.py
+++ systests/DistanceSensor/timeEasyDS.py
@@ -5,17 +5,17 @@ from __future__ import print_function
 from __future__ import division
 
 
-from di_sensors.distance_sensor import DistanceSensor
+from di_sensors.easy_distance_sensor import EasyDistanceSensor
 import time
 
-print("\nExample program for timing a single read of the Dexter Industries Distance Sensor.")
+print("\nExample program for timing a single read of the Dexter Industries Easy Distance Sensor.")
 
-ds = DistanceSensor()
+eds = EasyDistanceSensor()
 
 for i in range(10):
   # read the distance as a single-shot sample
   start=time.clock()
-  read_distance = ds.read_range_single()
+  read_distance = eds.read_mm()
   timing=time.clock() - start
   print("Distance from object: {} mm".format(read_distance))
   print("Timing: {:.3f}".format(timing))
