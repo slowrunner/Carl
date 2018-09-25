1257a6fdb4a913dc39624b68b70b09f61bf85734
slowrunner <slowrunner@users.noreply.gethub.com>
2018-09-25 18:48:17 -0400

    more stats

--- systests/DistanceSensor/timeEasyDS.py
+++ systests/DistanceSensor/timeEasyDS.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 #
-
+# timeEasyDS.py    Time EasyDistanceSensor.read_mm()
+#
+# Result: RPi3B  1.2MHz 4core   average 66ms 
+#
 from __future__ import print_function
 from __future__ import division
 
 
 from di_sensors.easy_distance_sensor import EasyDistanceSensor
 import time
+import numpy as np
 
 print("\nExample program for timing a single read of the Dexter Industries Easy Distance Sensor.")
 
 eds = EasyDistanceSensor()
-
-for i in range(10):
-  # read the distance as a single-shot sample
-  start=time.clock()
-  read_distance = eds.read_mm()
-  timing=time.clock() - start
-  print("Distance from object: {} mm".format(read_distance))
-  print("Timing: {:.3f}".format(timing))
-  time.sleep(1)
+timing_l = []
+dist_l = []
+delay_l = [0.0, 0.001, 0.005, 0.010, 0.100, 1.0]
+for delay in delay_l:
+  for i in range(10):
+    # read the distance as a single-shot sample
+    start=time.clock()
+    read_distance = eds.read_mm()
+    timing=time.clock() - start
+    timing_l += [timing]
+    dist_l += [read_distance]
+    print("Distance from object: {} mm".format(read_distance))
+    print("Timing: {:.3f}".format(timing))
+    time.sleep(delay)
+  mean_dist = np.mean(dist_l)
+  var_dist = np.var(dist_l)
+  print("\nDelay between readings: {:.0f}ms".format(delay*1000))
+  print("Distance Mean: {:.1f}mm Variance: {:.1f}mm  {:.1f}%".format(mean_dist,var_dist,var_dist/mean_dist*100))
+  print("EasyDistanceSensor.read_mm Average Time: {:.0f} ms\n\n".format(np.mean(timing_l)*1000))
--- systests/DistanceSensor/timeSingleShot.py
+++ systests/DistanceSensor/timeSingleShot.py
@@ -7,16 +7,21 @@ from __future__ import division
 
 from di_sensors.distance_sensor import DistanceSensor
 import time
+import numpy as np
 
 print("\nExample program for timing a single read of the Dexter Industries Distance Sensor.")
 
 ds = DistanceSensor()
+
+timing_l = []
 
 for i in range(10):
   # read the distance as a single-shot sample
   start=time.clock()
   read_distance = ds.read_range_single()
   timing=time.clock() - start
+  timing_l += [timing]
   print("Distance from object: {} mm".format(read_distance))
   print("Timing: {:.3f}".format(timing))
   time.sleep(1)
+print("\nAverage Time: {:.0f} ms".format(np.mean(timing_l)*1000))
