7b58459a56ca16924892a19de91d210157bdcc7b
slowrunner <slowrunner@users.noreply.gethub.com>
2019-01-16 20:51:06 -0500

    changed status to announce when battery is getting low

--- plib/status.py
+++ plib/status.py
@@ -4,16 +4,19 @@
 #      Run Battery down while printing status every 30s
 #
 # This test will loop reading the battery voltage
-#      UNTIL voltage stays below 8.5v 4 times,
+#      UNTIL voltage stays below 8.1v 4 times,
 #      then will issue a shutdown now
 #
 
 # After advice from some folks over at raspberrypi.org robotics forum,
 # and thinking about the 168 NiMH cells in my 10 year old Prius,
-# I have selected a shutdown limit of 1.1375v per cell, 9.1v at the battery,
-# which is 8.5v indicated by gopigo3.volt().
+# my target discharge depth is 15-20% remaining capacity.
+# 2019-January: ran logBattV.py - 8hrs to yellow light, a little more to "red and dead"
+# The GoPiGo3 volt() function registered 8.1v at the 15-20% capacity point 
+# which would be 8.7v at the battery pack or 1.09v/cell.
+# (GoPiGo3 reverse polarity protection diode drop of 0.6v)
 # This will yield a little more than 6.5 hours of mindless contemplation by my bot in its corner.
-# (Sacrificing 45 minutes of immediate gratification for longevity.)
+# Sacrificing 45-90 minutes of immediate gratification for longevity.
 
 #
 # from __future__ import print_function
@@ -34,7 +37,7 @@ from datetime import datetime
 import easygopigo3
 import battery
 
-LOW_BATTERY_V = 8.5   # 8cells x 1.1375 - 0.6 GoPiGo3 voltage drop
+LOW_BATTERY_V = 8.1   # (8cells x 1.09) - 0.6 GoPiGo3 voltage drop from reverse voltage protection diode
 
 # Return CPU temperature as a character string
 def getCPUtemperature():
@@ -108,6 +111,8 @@ def main():
         vBatt = egpg.volt()
         if (vBatt < LOW_BATTERY_V):
             batteryLowCount += 1
+            speak.say("Hello? My battery is getting a little low here.")
+            print("\nHello? My Battery is getting a little low here.")
         else: batteryLowCount = 0
         if (batteryLowCount > 3):
           speak.say("WARNING, WARNING, SHUTTING DOWN NOW")
