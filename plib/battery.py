75052e407fe42c003d3e93badd727ac7800a4a7e
slowrunner <slowrunner@users.noreply.github.com>
2019-01-16 20:14:20 -0500

    update battery.py for 2019 life

--- plib/battery.py
+++ plib/battery.py
@@ -10,7 +10,8 @@ sys.path.append("/home/pi/Carl/plib")
 import time
 import os
 
-SafeShutDown = 8.75  # (1.09375v/cell yields 8h "fun time", 14% life remaining 1h18m)
+#  8*1.09v = 8.72v - 0.6v GoPiGo3 reverse polarity protection diode to get volt() reading
+SafeShutDown = 8.1  # (1.09v/cell yields 6.5hr "fun time", 15-20% life remaining)
 
 # ########## HOURS OF LIFE REMAINING
 # hoursOfLifeRemaining(Vbatt)
@@ -22,24 +23,24 @@ SafeShutDown = 8.75  # (1.09375v/cell yields 8h "fun time", 14% life remaining 1
 #      number of recharge cycles.
 #
 # Historical:
+#   Jan 2018  8h07m
 #   Sep 2018  9h21m   (8x EBL 2800mAh AA cells, around 2550 mAh on BC-3000 test)
 #   Aug 2018  7h20m   (6xEnergizer 2300mAh AA and 2xAmazon 2000mAh AA)
 
 #  (V , Time remaining)
 
 lifePoints= (
- (16.0,  8.30),   # charging
- (11.0,  8.25),   # guess
- (10.23, 7.95),   # 7h 57m to 8.75, 9h21m to 7.4v
- (9.56,  6.95),
- (9.40,  5.95),
- (9.33,  4.95),
- (9.28,  3.95),
- (9.22,  1.95),
- (8.95,  0.95),
- (8.75,  0.00),
- (7.99, -1.30),
- (7.40, -1.38),
+ (16.0,  8.50),   # charging
+ (11.0,  8.25),   # 
+ (10.23, 8.12),   # 8h 07m to 7.04v
+ (9.17,  7.12),
+ (8.75,  6.12),
+ (8.54,  5.12),
+ (8.42,  4.12),
+ (8.33,  3.12),
+ (8.19,  2.12),
+ (7.94,  1.12),
+ (7.04,  0.12),
  (0.00, -2.00)
  )
 
@@ -67,7 +68,7 @@ def main():
       print "\n"
       printLifeTable()
       print "batteryTooLow(9.0): ",batteryTooLow(9.0)
-      print "batteryTooLow(8.74): ",batteryTooLow(8.74)
+      print "batteryTooLow(8.0): ",batteryTooLow(8.0)
       Vtest=10.25
       print "hoursOfLifeRemaining(%.1f):%.2fh"% (Vtest,hoursOfLifeRemaining(Vtest)) 
 
