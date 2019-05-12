8aa274ae685d880f70b72fc801e1a244c972e3b1
slowrunner <slowrunner@users.noreply.gethub.com>
2019-05-12 09:13:52 -0400

    working

--- Examples/audio/clap.py
+++ Examples/audio/clap.py
@@ -2,11 +2,26 @@
 
 # from https://github.com/nikhiljohn10/pi-clap
 
+# to install pyaudio
+# sudo pip install portaudio19-dev
+# sudo pip install pyaudio
+
+# ignore all the alsa configuration errors - it will work
+# had to set exception_on_overflow = False on the read or always got exception
+
+
 import pyaudio
 import sys
 import _thread
 from time import sleep
 from array import array
+
+try:
+	sys.path.append('/home/pi/Carl/plibx')
+	import speak
+	Carl = True
+except:
+	Carl = False
 
 clap = 0
 wait = 2
@@ -16,6 +31,8 @@ exitFlag = False
 
 def toggleLight():
 	print("Light toggled")
+	if Carl:
+		speak.say("Yes master, I heard you.")
 	sleep(1)
 
 def waitForClaps(threadName):
@@ -28,9 +45,10 @@ def waitForClaps(threadName):
 	if clap == 2:
 		print("Two claps")
 		toggleLight()
-	# elif clap == 3:
-	# 	print "Three claps"
+	elif clap == 3:
+	 	print("Three claps")
 	elif clap == 4:
+		print("Four claps")
 		exitFlag = True
 	print("Claping Ended")
 	clap = 0
@@ -53,10 +71,11 @@ def main():
 					input=True,
 					output=True,
 					frames_per_buffer=chunk)
+	print("mic sample rate:",p.get_device_info_by_index(0)['defaultSampleRate'])
 	try:
 		print("Clap detection initialized")
 		while True:
-			data = stream.read(chunk)
+			data = stream.read(chunk, exception_on_overflow = False)
 			as_ints = array('h', data)
 			max_value = max(as_ints)
 			if max_value > threshold:
