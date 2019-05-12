95a4e368e7c1482ca87b7b732cb3f8012d28768d
slowrunner <slowrunner@users.noreply.gethub.com>
2019-05-12 09:27:47 -0400

    detect non-Carl execution

--- Examples/templates/carl_template.py
+++ Examples/templates/carl_template.py
@@ -11,26 +11,30 @@ Documentation:
 # from __future__ import division       #                           ''
 
 import sys
-sys.path.append('/home/pi/Carl/plib')
+try:
+    sys.path.append('/home/pi/Carl/plibx')
+    import speak
+    import tiltpan
+    import status
+    import battery
+    import myDistSensor
+    import lifeLog
+    Carl = True
+except:
+    Carl = False
 import easygopigo3 # import the GoPiGo3 class
-import tiltpan
-#import status
-#import battery
 import numpy as np
 import datetime as dt
-#import speak
-#import myDistSensor
-import lifeLog
 import argparse
 from time import sleep
 # import cv2
 
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--file", help="path to input file")
-ap.add_argument("-n", "--num", type=int, default=5, help="number")
-args = vars(ap.parse_args())
-print("Started with args:",args)
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-f", "--file", required=True, help="path to input file")
+# ap.add_argument("-n", "--num", type=int, default=5, help="number")
+# args = vars(ap.parse_args())
+# print("Started with args:",args)
 
 
 # constants
@@ -40,17 +44,19 @@ print("Started with args:",args)
 
 
 def main():
-    lifeLog.logger.info("Started")
+    if Carl: lifeLog.logger.info("Started")
     egpg = easygopigo3.EasyGoPiGo3(use_mutex=True)
-    tiltpan.tiltpan_center()
-    sleep(0.5)
-    tiltpan.off()
+    if Carl: 
+        tiltpan.tiltpan_center()
+        sleep(0.5)
+        tiltpan.off()
 
     try:
         #  loop
         loopSleep = 1 # second
         loopCount = 0
-        while True:
+        keepLooping = True
+        while keepLooping:
             loopCount += 1
 
 
@@ -61,14 +67,8 @@ def main():
             print("\n*** Ctrl-C detected - Finishing up")
             sleep(1)
     if (egpg != None): egpg.stop()
-    lifeLog.logger.info("Finished")
+    if Carl: lifeLog.logger.info("Finished")
     sleep(1)
 
-if __name__ == "__main__":
-	main()
 
-
-
-
-
-if (__name__ == 'main'):  main()
+if (__name__ == '__main__'):  main()
