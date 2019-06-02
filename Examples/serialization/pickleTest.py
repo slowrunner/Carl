7c10e1a2ec238df5fddd2b90ead8fdd2e7d8d88b
slowrunner <slowrunner@users.noreply.gethub.com>
2019-06-01 21:20:48 -0400

    learning

---
+++ Examples/serialization/pickleTest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# file:  pickleTest.py
+#
+# Serialize a value to a file, and deserialize it the next run
+#
+
+import pickle
+
+
+try:
+    chargeCycles = pickle.load(open('chargeCycles.pkl', 'rb'))
+
+except:
+    chargeCycles = 0
+
+
+print("chargeCycles:",chargeCycles)
+
+chargeCycles += 1
+pickle.dump(chargeCycles, open('chargeCycles.pkl', 'wb'))
+
+print("pickled chargeCycles:",chargeCycles)
+
