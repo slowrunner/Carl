dbbedac5a300c462f64279cb094d2b11bdfc4062
slowrunner <slowrunner@users.noreply.gethub.com>
2019-06-21 09:31:41 -0400

    formatting

--- totallife.sh
+++ totallife.sh
@@ -3,5 +3,5 @@
 # totallife.sh    print total hours of life in life.log 
 #
 
-awk -F':' '{sum+=$3}END{print "total life: " sum "hrs";}' life.log
+awk -F':' '{sum+=$3}END{print "total life: " sum " hrs";}' life.log
 
