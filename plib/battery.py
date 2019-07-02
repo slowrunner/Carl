67c997a75f8a4e8f73efc44250acaed1e2d81345
slowrunner <slowrunner@users.noreply.gethub.com>
2019-07-02 09:16:35 -0400

    new batteries

--- plib/battery.py
+++ plib/battery.py
@@ -10,8 +10,8 @@ sys.path.append("/home/pi/Carl/plib")
 import time
 import os
 
-#  8*1.09v = 8.72v - 0.6v GoPiGo3 reverse polarity protection diode to get volt() reading
-SafeShutDown = 8.1  # (1.09v/cell yields 6.5hr "fun time", 15-20% life remaining)
+#  8 * 1.0125v/cell = 8.1v - 0.6v GoPiGo3 reverse polarity protection diode to get volt() reading
+SafeShutDown = 7.5  # (1.0125v/cell yields 6.7hr "fun time", 2% life 8m remaining)
 
 # ########## HOURS OF LIFE REMAINING
 # hoursOfLifeRemaining(Vbatt)
@@ -23,29 +23,30 @@ SafeShutDown = 8.1  # (1.09v/cell yields 6.5hr "fun time", 15-20% life remaining
 #      number of recharge cycles.
 #
 # Historical:
-#   Apr 2019  Auto docking off at trickle, on at 8.5v mean
+#   Jul 2019  7h to death, 6h15m to 8.1v playtime (8x Powerowl 2800mAh 2250 on BC-3000 test)
+#   Apr 2019  Auto docking off at trickle, on at 8.5v mean (new EBLs)
 #   Apr 2019  7h49m to 8.5v, +30m to 8.1, +15m to 7.5v, +1m to 7.1v h/w shutdown
 #   Jan 2018  8h07m
 #   Sep 2018  9h21m   (8x EBL 2800mAh AA cells, around 2550 mAh on BC-3000 test)
-#   Aug 2018  7h20m   (6xEnergizer 2300mAh AA and 2xAmazon 2000mAh AA)
-#   Mar 2019  1h30m   and 16.45v charging seen
+#   Aug 2018  7h20m   (8x EBL 2800mAh))
+
 
 #  (V , Time remaining)
-
 lifePoints= (
- (18.000, 8.00),   # charging
- (11.025, 7.82),   # 
- (9.688,  6.82),   # 8h 48m to 8.5v
- (9.483,  5.82),
- (9.346,  4.82),
- (9.286,  3.82),
- (9.200,  2.82),
- (9.046,  1.82),
- (8.806,  0.82),
- (8.500,  0.00),   # 45m to 7.5v shutdown, recharge now
- (8.100,  -0.50),   # 15m to 7.5v shutdown
- (7.500,  -0.53),   # 1-2 min to total die
- (0.000,  -99.00)
+ (18.00,  7.02),   # charging
+ (11.00,  7.01),   #
+ (10.70,  7.00),   # 7h to total die
+ (9.580,  6.00),
+ (9.400,  5.00),
+ (9.310,  4.00),
+ (9.250,  3.00),
+ (9.140,  2.00),
+ (8.810,  1.00),
+ (8.100,  0.45),   # 27m or 20m to 7.5v shutdown - normal playtime limit
+ (7.700,  0.22),   # 13m or 5m to 7.5v shutdown - conditioning limit
+ (7.500,  0.13),   # 8 min estimated to total die
+ (7.200,  0.00),
+ (0.000,  -999.00)
  )
 
 hoursOfLifeRemainingArray = myPyLib.InterpolatedArray(lifePoints)
@@ -71,10 +72,11 @@ def batteryTooLow(vBatt):
 def main():
       print ("\n")
       printLifeTable()
-      print ("batteryTooLow(9.0): ",batteryTooLow(9.0))
-      print ("batteryTooLow(8.0): ",batteryTooLow(8.0))
-      Vtest=10.25
-      print ("hoursOfLifeRemaining(%.1f):%.2fh"% (Vtest,hoursOfLifeRemaining(Vtest)) )
+      print ("Safety Shutdown:",SafeShutDown,"v")
+      print ("batteryTooLow(8.5): ",batteryTooLow(8.5))
+      print ("batteryTooLow(7.4): ",batteryTooLow(7.4))
+      Vtest=10.7
+      print ("hoursOfLifeRemaining(%.1f): %.2fh"% (Vtest,hoursOfLifeRemaining(Vtest)) )
 
 if __name__ == "__main__":
     main()
