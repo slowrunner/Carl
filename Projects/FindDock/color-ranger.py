3ecf6536aaa3b18a57321faa7aa1dab714104ce3
slowrunner <slowrunner@users.noreply.gethub.com>
2019-07-04 08:49:25 -0400

    color-ranger utility update

---
+++ Projects/FindDock/color-ranger.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# File color-ranger.py
+#
+# based on Practical Python and OpenCV chapter 3 program range-detector.py
+#
+# Caution: This process takes 60% of RPi 3 and load average of 3.5
+#          and will send processor temp to 78C, almost to temp threshold
+#          DO NOT LEAVE RUNNING on GoPiGo3 for long
+#
+# USAGE: You need to specify a filter and "only one" image source
+#
+# (python) color-ranger.py --filter RGB --image /path/to/image.png
+# or
+# (python) color-ranger.py --filter HSV --webcam
+
+import cv2
+import argparse
+from operator import xor
+import imutils
+
+def callback(value):
+    pass
+
+
+def setup_trackbars(range_filter):
+    cv2.namedWindow("Trackbars", 0)
+
+    for i in ["MIN", "MAX"]:
+        v = 0 if i == "MIN" else 255
+
+        for j in range_filter:
+            cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
+
+
+def get_arguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', '--filter', required=True,
+                    help='Range filter. RGB or HSV')
+    ap.add_argument('-i', '--image', required=False,
+                    help='Path to the image')
+    ap.add_argument('-w', '--webcam', required=False,
+                    help='Use webcam', action='store_true')
+    ap.add_argument('-p', '--preview', required=False,
+                    help='Show a preview of the image after applying the mask',
+                    action='store_true')
+    args = vars(ap.parse_args())
+
+    if not xor(bool(args['image']), bool(args['webcam'])):
+        ap.error("Please specify only one image source")
+
+    if not args['filter'].upper() in ['RGB', 'HSV']:
+        ap.error("Please speciy a correct filter, -f RGB or HSV.")
+
+    return args
+
+
+def get_trackbar_values(range_filter):
+    values = []
+
+    for i in ["MIN", "MAX"]:
+        for j in range_filter:
+            v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
+            values.append(v)
+
+    return values
+
+
+def main():
+
+  try:
+    args = get_arguments()
+
+    range_filter = args['filter'].upper()
+
+    if args['image']:
+        image = cv2.imread(args['image'])
+
+        if range_filter == 'RGB':
+            frame_to_thresh = image.copy()
+        else:
+            frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    else:
+        camera = cv2.VideoCapture(0)
+
+    setup_trackbars(range_filter)
+
+    print("\n\nPRESS   Q   TO QUIT")
+
+    while True:
+        if args['webcam']:
+            ret, image = camera.read()
+
+            if not ret:
+                break
+
+            if range_filter == 'RGB':
+                frame_to_thresh = image.copy()
+            else:
+                frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+
+        thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+
+        if args['preview']:
+            preview = cv2.bitwise_and(image, image, mask=thresh)
+            cv2.imshow("Preview", preview)
+        else:
+            cv2.imshow("Original", imutils.resize(image, width=600))
+            cv2.imshow("Thresh", imutils.resize(thresh, width=600))
+
+        if cv2.waitKey(1) & 0xFF is ord('q'):
+            break
+  except KeyboardInterrupt:
+    print("\nExiting color-ranger.py")
+
+if __name__ == '__main__':
+    main()
+
