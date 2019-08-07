d2039c8adf30eaaf1d65a5a185ae405c594b3da1
slowrunner <slowrunner@users.noreply.gethub.com>
2019-08-07 15:26:37 -0400

    new leds -s on|off functionality

--- plib/leds.py
+++ plib/leds.py
@@ -5,13 +5,25 @@
 # Methods:
 #    leds.all_on(egpg)   turn two red blinker leds on and two "eyes" on bright white
 #    leds.all_off(egpg)  turn two red blinker leds off and two "eyes" off
-
+#
+# Usage:
+#    import leds
+#    egpg=easygopigo3.EasyGoPiGo3()
+#    leds.all_on(egpg)
+#    leds.all_off(egpg)
+#
+#    or from command line:
+#    > ./leds.py          performs test on/off
+#    > ./leds.py -s on    turns leds on
+#    > ./leds.py -s off   turns leds off
+#
 
 import easygopigo3
 import sys
 sys.path.append("/home/pi/Carl/plib")
 import myconfig
 from time import sleep
+import argparse
 
 WHITE_BRIGHT = (255, 255, 255)
 
@@ -32,13 +44,23 @@ def all_off(egpg=None):
 
 
 def main():
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--set", action="store", default=None, help="set all leds 'on' or 'off'")
+    args = vars(ap.parse_args())
     egpg = easygopigo3.EasyGoPiGo3(use_mutex=True)
     myconfig.setParameters(egpg)
+    set = args["set"]
 
-    print("leds.py: Test all_on()")
-    all_on(egpg)
-    sleep(5)
-    print("leds.py: Test all_off()")
-    all_off(egpg)
+    if set==None:
+        print("leds.py: Test all_on()")
+        all_on(egpg)
+        sleep(5)
+        print("leds.py: Test all_off()")
+        all_off(egpg)
+    elif set=='on':
+        all_on(egpg)
+    else:
+        all_off(egpg)
 
 if (__name__ == '__main__'): main()
