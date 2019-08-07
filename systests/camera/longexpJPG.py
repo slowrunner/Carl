ad240985d5f3de2be75ac9d83a6ae4b5f8d69d5e
slowrunner <slowrunner@users.noreply.gethub.com>
2019-08-07 15:28:10 -0400

    added leds.all_on longExp with LEDs

--- systests/camera/longexpJPG.py
+++ systests/camera/longexpJPG.py
@@ -8,8 +8,25 @@ from datetime import datetime
 import sys
 sys.path.append("/home/pi/Carl/plib")
 import camUtils
+import leds
+import easygopigo3
+import myconfig
 
+egpg = easygopigo3.EasyGoPiGo3()
+myconfig.setParameters(egpg)
+
+
+print("TEST LONG EXPOSURE WITH NO EXTRA LEDS")
 filename = "longExp_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".jpg"
 fn = camUtils.snapJPG(fpath="/home/pi/Carl/images",fname=filename,longexp=True)
 print("wrote ",fn)
+
+
+print('TEST LONG EXPOSURE WITH "EYES" AND "BLINKER" LEDS ON')
+leds.all_on(egpg)
+filename = "longWleds_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".jpg"
+fn = camUtils.snapJPG(fpath="/home/pi/Carl/images",fname=filename,longexp=True)
+leds.all_off(egpg)
+print("wrote ",fn)
+
 
