72af5a77bf0a2fe5055ba5b04bcf141e612f3226
slowrunner <slowrunner@users.noreply.gethub.com>
2019-09-08 09:14:52 -0400

    servo example

---
+++ Examples/cpp/Examples/servos.cpp
@@ -0,0 +1,45 @@
+/*
+ *  https://www.dexterindustries.com/GoPiGo3/
+ *  https://github.com/DexterInd/GoPiGo3
+ *
+ *  Copyright (c) 2017 Dexter Industries
+ *  Released under the MIT license (http://choosealicense.com/licenses/mit/).
+ *  For more information see https://github.com/DexterInd/GoPiGo3/blob/master/LICENSE.md
+ *
+ *  This code is an example for using GoPiGo3 servos.
+ *
+ *  Results:  When you run this program, you should see the servos cycle.
+ *
+ *  To compile see: https://github.com/slowrunner/Carl/blob/master/Examples/cpp/Alan_Note.txt
+ */
+
+#include <GoPiGo3.h>   // for GoPiGo3
+#include <stdio.h>     // for printf
+#include <unistd.h>    // for usleep
+#include <signal.h>    // for catching exit signals
+
+GoPiGo3 GPG;
+
+void exit_signal_handler(int signo);
+
+int main(){
+  signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
+
+  GPG.detect(); // Make sure that the GoPiGo3 is communicating and that the firmware is compatible with the drivers.
+
+  // Reset the servos to center
+  GPG.set_servo(SERVO_1,1500);
+  GPG.set_servo(SERVO_2,1500);
+
+  while(true){
+    usleep(20000);
+  }
+}
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+  if(signo == SIGINT){
+    GPG.reset_all();    // Reset everything so there are no run-away motors
+    exit(-2);
+  }
+}
