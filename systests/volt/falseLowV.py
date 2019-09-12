605e0b5a556dc7460a5512f7a8412d6a3dffef9e
slowrunner <slowrunner@users.noreply.gethub.com>
2019-09-12 18:34:41 -0400

    update

---
+++ systests/volt/falseLowV.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+#
+# falseLowV.py
+
+"""
+Documentation:
+
+Test to see if EasyGoPiGo3.volt() ever returns a ridiculously low voltage
+"""
+
+# from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+# from __future__ import division       #                           ''
+
+import sys
+try:
+    sys.path.append('/home/pi/Carl/plib')
+    # import speak
+    # import tiltpan
+    # import status
+    # import battery
+    # import myDistSensor
+    # import lifeLog
+    # import runLog
+    import myconfig
+    # import myimutils   # display(windowname, image, scale_percent=30)
+    Carl = True
+except:
+    Carl = False
+import easygopigo3 # import the EasyGoPiGo3 class
+import numpy as np
+# import datetime as dt
+# import argparse
+from time import sleep
+
+# import cv2
+
+# ARGUMENT PARSER
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-f", "--file", required=True, help="path to input file")
+# ap.add_argument("-n", "--num", type=int, default=5, help="number")
+# ap.add_argument("-l", "--loop", default=False, action='store_true', help="optional loop mode")
+# args = vars(ap.parse_args())
+# print("Started with args:",args)
+# filename = args['file']
+# loopFlag = args['loop']
+
+# CONSTANTS
+
+
+# VARIABLES
+
+
+# METHODS 
+
+
+
+# MAIN
+
+def main():
+    # if Carl: runLog.logger.info("Started")
+    try:
+        egpg = easygopigo3.EasyGoPiGo3(use_mutex=True)
+    except:
+        strToLog = "Could not instantiate an EasyGoPiGo3"
+        print(strToLog)
+        if Carl: lifeLog.logger.info(strToLog)
+        exit(1)
+    if Carl:
+        myconfig.setParameters(egpg)
+        # tp = tiltpan.TiltPan(egpg)
+        # tp.tiltpan_center()
+        # tp.off()
+
+    try:
+        # Do Somthing in a Loop
+        loopSleep = 0.005 # second
+        loopCount = 0
+        keepLooping = True
+        print("Starting test at {}v".format(egpg.volt()))
+        while keepLooping:
+            loopCount += 1
+            # do something
+            v = egpg.volt()
+            if v < 7:  print("Low V: {} on loop: {}".format(v, loopCount))
+            sleep(loopSleep)
+
+        # Do Something Once
+
+
+    except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+       	    if (egpg != None): egpg.stop()           # stop motors
+            print("\n*** Ctrl-C detected - Finishing up")
+            sleep(1)
+    if (egpg != None): egpg.stop()
+    # if Carl: runLog.logger.info("Finished")
+    sleep(1)
+
+
+if (__name__ == '__main__'):  main()
