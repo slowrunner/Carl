bfef952a5a933d393282fa40c531c5a08775c35d
slowrunner <slowrunner@users.noreply.gethub.com>
2019-09-22 22:45:06 -0400

    back to espeak

--- plib/speak.py
+++ plib/speak.py
@@ -17,6 +17,7 @@ import subprocess
 import sys
 import time
 debug = False
+import math
 
 # QUIET TIME is before 10AM and after 10PM 
 # (unless told to ignore , then never quietTime
@@ -32,6 +33,31 @@ def quietTime(startOK=10,notOK=23,ignore=False):
         return False
     else:
         return True
+
+# used when espeak was broke
+def say_flite(phrase,vol=100,anytime=False):
+    phrase = phrase.replace("I'm","I m")
+    phrase = phrase.replace("'","")
+    phrase = phrase.replace('"',' quote ')
+    phrase = phrase.replace('*',"")
+
+
+    # flite volume is double millibels from 0 to -6000
+    # whisper should be around 35-40%
+    # say/normal volume is around 80
+    # shout is like 100 to 150, distorts at 170
+
+    YYY = int(2000 * (math.log(int(vol)/100.0)))
+
+    if (quietTime(ignore=anytime)):
+        print("QuietTime speak request: {} at vol: {}".format(phrase,vol))
+    else:
+        try:
+            subprocess.check_output(['flite -t "%s" -o tmp.wav' % phrase], stderr=subprocess.STDOUT, shell=True)
+            subprocess.check_output(['omxplayer --vol "%d" tmp.wav' % YYY], stderr=subprocess.STDOUT, shell=True)
+            subprocess.check_output(['rm tmp.wav'], stderr=subprocess.STDOUT, shell=True)
+        except KeyboardInterrupt:
+            sys.exit(0)
 
 # Speak a phrase using espeak
 # Options: vol: 10 is whisper, 50 is "normal Carl", 200 is shouting, 500 is screaming
@@ -53,12 +79,18 @@ def say_espeak(phrase,vol=100,anytime=False):
 
 def say(phrase,vol=50,anytime=False):
     say_espeak(phrase,vol,anytime)
+    # vol = vol + 40  # adjust for flite
+    # say_flite(phrase,vol,anytime)
 
 def shout(phrase,vol=200,anytime=False):
     say_espeak(phrase,vol,anytime)
+    # vol = vol - 50  # adjust for flite
+    # say_flite(phrase,vol,anytime)
 
 def whisper(phrase,vol=10,anytime=True):
     say_espeak(phrase,vol,anytime)
+    # vol = vol + 30  # adjust for flite
+    # say_flite(phrase,vol,anytime=False)
 
 # ##### MAIN ####
 def main():
@@ -78,10 +110,10 @@ def main():
         say(strToSay,vol,ignore)
     else:
         debug = True
-        say("This phrase contained an apostrophe which isn't allowed")
-        whisper('This phrase contains "a quoted word" ', 20)
-        shout("My name is Carl. ")
-        say("I don't know Pogo.  Never met the little bot")
+        say("Just saying. This phrase contained an apostrophe which isn't allowed")
+        whisper('I need to whisper.  This phrase contains "a quoted word" ')
+        shout("I feel like shouting.  My name is Carl. ")
+        whisper("Whisper at 20. I don't know Pogo.  Never met the little bot",20,True)
 
 if __name__ == "__main__":
     main()
