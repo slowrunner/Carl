5db8948f091fff3cbcf9e1cd03531adc33551275
slowrunner <slowrunner@users.noreply.gethub.com>
2020-01-10 17:37:27 -0500

    totals

--- stats.year.sh
+++ stats.year.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 #
-# totallife.sh    print total hours and sessions of life in life.log
+# stats.year.sh    print total hours and sessions of life in life.log.year.1
 #
 # requires bc  (sudo apt-get install bc)
 #
-declare -i newBattsAtCycle=486
+fn='life.log.year.1'
+# declare -i newBattsAtCycle=233
+#
 # awk -F':' '{sum+=$3}END{print "total life: " sum " hrs";}' life.log
-totalLife=`(awk -F':' '{sum+=$3}END{print sum;}' life.log)`
+totalLife=`(awk -F':' '{sum+=$3}END{print sum;}' $fn)`
 echo "Total Life: " $totalLife "hrs"
-lifeThisYear=`(awk -F':' 'FNR > 6 {sum+=$3}END{print sum;}' life.log)`
+lifeThisYear=`(awk -F':' 'FNR > 6 {sum+=$3}END{print sum;}' $fn)`
 echo "Life this year: " $lifeThisYear "hrs (BOY Aug 22)"
-# echo "Sessions (boot) this year: " `(grep -c "\- boot \-" life.log)`
-bootedThisYr=`(grep "\- boot \-" life.log | sort -u -k1,1 | wc -l)`
+# echo "Sessions (boot) this year: " `(grep -c "\- boot \-" $fn)`
+bootedThisYr=`(grep "\- boot \-" $fn | sort -u -k1,1 | wc -l)`
 echo "Days Booted This Year: " $bootedThisYr
 aveSession=`(echo "scale=0; $lifeThisYear / $bootedThisYr" | bc)`
 echo "Average Time Between Reboot: " $aveSession "hrs"
-lastDockingStr=`(grep "h playtime" life.log | tail -1)`
+lastDockingStr=`(grep "h playtime" $fn | tail -1)`
 totalDockings=`(awk -F"Docking " '{sub(/ .*/,"",$2);print $2}' <<< $lastDockingStr)`
 echo "Total Dockings: " $totalDockings
-dockingsThisYear=`(grep -c "h playtime" life.log)`
+dockingsThisYear=`(grep -c "h playtime" $fn)`
 echo "Dockings this year: " $dockingsThisYear
-echo "New Batteries At Cycle:" $newBattsAtCycle
-currentBattCycles=`(echo "scale=1; $totalDockings - $newBattsAtCycle" | bc)`
-echo "Battery Set At Cycle: " $currentBattCycles
-dockingFailures=`(grep -c "Docking Failure Possible" life.log)`
+# echo "New Batteries At Cycle:" $newBattsAtCycle
+# currentBattCycles=`(echo "scale=1; $totalDockings - $newBattsAtCycle" | bc)`
+# echo "Battery Set At Cycle: " $currentBattCycles
+dockingFailures=`(grep -c "Docking Failure Possible" $fn)`
 failurePercent=`(echo "scale=1; $dockingFailures * 100 / $dockingsThisYear" | bc)`
 echo "Docking Failures this year: " $dockingFailures " or " $failurePercent "% of Dockings"
-#aveCycleTimeTotal=`(echo "scale=1; $totalLife / $totalDockings" | bc)`
-#echo "Ave Cycle total life: " $aveCycleTimeTotal "hours"
+# aveCycleTimeTotal=`(echo "scale=1; $totalLife / $totalDockings" | bc)`
+# echo "Ave Cycle total life: " $aveCycleTimeTotal "hours"
 aveCycleTime=`(echo "scale=1; $lifeThisYear / ($dockingsThisYear - $dockingFailures)" | bc)`
 echo "Ave Cycle this year (w/o failures): " $aveCycleTime "hours"
-
-playtime=`(grep playtime life.log | awk -F"after" '{sum+=$2}END{print sum;}' )`
+#
+playtime=`(grep playtime $fn | awk -F"after" '{sum+=$2}END{print sum;}' )`
 avePlaytime=`(echo "scale=1; $playtime / ($dockingsThisYear - $dockingFailures)" | bc)`
-rechargeTime=`(grep recharge life.log | awk -F"after" '{sum+=$2}END{print sum;}' )`
+rechargeTime=`(grep recharge $fn | awk -F"after" '{sum+=$2}END{print sum;}' )`
 aveRechargeTime=`(echo "scale=1; $rechargeTime / ($dockingsThisYear - $dockingFailures)" | bc)`
 aveCycleTime=`(echo "scale=1; $avePlaytime + $aveRechargeTime" | bc)`
 echo "Ave Cycle this year: " $aveCycleTime "hours"
 echo "Ave Playtime this year: " $avePlaytime
 echo "Last Docking: " $lastDockingStr
-echo "Last Recharge: " `(grep Dismount life.log | tail -1)`
+echo "Last Recharge: " `(grep Dismount $fn | tail -1)`
