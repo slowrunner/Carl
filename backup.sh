851ef22764ce91abc7886af5804200bf285b19c2
slowrunner <slowrunner@users.noreply.gethub.com>
2020-10-24 09:36:01 -0400

    switch to PiOS

--- backup.sh
+++ backup.sh
@@ -4,6 +4,7 @@ cp life.log life.log.backup
 cp wheel.log wheel.log.backup
 cp imu.log imu.log.backup
 cp run.log run.log.backup
-echo "backed up life, wheel, imu, and run logs"
+cp carlData.json carlData.json.backup
+echo "backed up life, wheel, imu, run logs and carlData.json"
 ls -al *.backup
 
