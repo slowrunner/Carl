6495c791cc6c4413c45ba19e8872285e3f7f73be
slowrunner <slowrunner@users.noreply.github.com>
2020-10-25 11:07:51 -0400

    enhanced

--- wifi_led_off.sh
+++ wifi_led_off.sh
@@ -2,6 +2,11 @@
 
 # stop GoPiGo3 antenna_wifi.service
 sudo systemctl stop antenna_wifi
+
+# Stop the GoPiGo3 antenna_wifi.service being loaded at boot 
+sudo systemctl disable antenna_wifi
+
 # turn off wifi_led
 python -c "import gopigo3;GPG=gopigo3.GoPiGo3();GPG.set_led(GPG.LED_WIFI,0,0,0)"
-
+# add notice to life.log
+/home/pi/Carl/logMaintenance.py "WiFi LED (antenna_wifi service) turned off permenantly"
--- wifi_led_on.sh
+++ wifi_led_on.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# start GoPiGo3 antenna_wifi.service
+# Set the GoPiGo3 antenna_wifi.service to start at boot
+sudo systemctl enable antenna_wifi
+
+
+# start GoPiGo3 antenna_wifi.service now
 sudo systemctl start antenna_wifi
+
+# add notice to life.log
+/home/pi/Carl/logMaintenance.py "WiFi LED (antenna_wifi service) turned on permenantly"
+
 
