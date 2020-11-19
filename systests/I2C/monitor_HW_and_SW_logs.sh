f55fbe3fe5c01c30948dee7b857041bff2c29eff
slowrunner <slowrunner@users.noreply.github.com>
2020-11-19 11:24:56 -0500

    fix log file names

--- systests/I2C/monitor_HW_and_SW_logs.sh
+++ systests/I2C/monitor_HW_and_SW_logs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while true; do vcgencmd measure_temp; uptime; tail HW_I2C_during_SW_I2C_Stress.log; echo -e '\n======'; tail SW_I2C_Stress2.log; echo -e '\n====='; grep -c Exception SW_I2C_Stress2.log;echo -e "IMU (SW I2C) Exceptions \n=====";sleep 5; done;
+while true; do vcgencmd measure_temp; uptime; tail HW_I2C_during_SW_I2C_Stress.log; echo -e '\n======'; tail SW_I2C_Stress.log; echo -e '\n====='; grep -c Exception SW_I2C_Stress.log;echo -e "IMU (SW I2C) Exceptions \n=====";sleep 5; done;
