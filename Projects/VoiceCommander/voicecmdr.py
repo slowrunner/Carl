32bdf2b462361b020e915e6b9ba9b70cd8b253dc
slowrunner <slowrunner@users.noreply.github.com>
2021-01-13 16:16:30 -0500

    nlu mode, match vcommand.py

--- Projects/VoiceCommander/voicecmdr.py
+++ Projects/VoiceCommander/voicecmdr.py
@@ -44,86 +44,140 @@ USAGE:  ./voicecmdr.py
 import time
 import sys
 import random
+import traceback
 
-# add Carl's plib to path
-sys.path.append("/home/pi/Carl/plib")
+# add Carl's plib to path and use plib easygopigo3
+sys.path.insert(1,"/home/pi/Carl/plib")
 
-import noinit_easygopigo3
+import easygopigo3
 import hotword
+import vcommand
+import eyes
+import tiltpan
+import speak
 
-
-
-# ==== GoPiGo3 Eye Management =====
-
-EYE_COLOR_COMMANDABLE = ( 0, 0,255)    # BRIGHT_BLUE
-EYE_COLOR_HOTWORD     = ( 0, 0, 20)    # LIGHT_BLUE
-EYE_COLOR_ACCEPTED    = ( 0,20,  0)    # LIGHT_GREEN
-EYE_COLOR_REJECTED    = (20, 0,  0)    # LIGHT_RED
-EYE_COLOR_OFF         = ( 0, 0,  0)    # OFF
-
-def carl_eyes(egpg,newState):
-	if newState == None:
-		try:
-			newState = egpg._carl_eyes
-			if newState == EYE_COLOR_OFF:
-				newState = False
-		except:
-			egpg._carl_eyes = EYE_COLOR_OFF
-			egpg.close_eyes()
-			newState = False
-
-	else:
-		egpg._carl_eyes = newState
-		if newState == EYE_COLOR_OFF:
-			egpg.close_eyes()
-			newState = False
-		else:
-			egpg.set_eye_color(egpg._carl_eyes)
-			egpg.open_eyes()
-
-
-	# returns either False or color tuple
-	return newState
-
+"""
+eyes.EYE_COLOR_COMMANDABLE = ( 0, 0,255)    # BRIGHT_BLUE
+eyes.EYE_COLOR_HOTWORD     = ( 0, 0, 20)    # LIGHT_BLUE
+eyes.EYE_COLOR_NL          = (50,50, 50)    # LIGHT_WHITE
+eyes.EYE_COLOR_ACCEPTED    = ( 0,20,  0)    # LIGHT_GREEN
+eyes.EYE_COLOR_REJECTED    = (20, 0,  0)    # LIGHT_RED
+eyes.EYE_COLOR_OFF         = ( 0, 0,  0)    # OFF
+"""
 
 # ==== voicecmdr.py  MAIN ====
 
 def main():
-
-	egpg = noinit_easygopigo3.EasyGoPiGo3(use_mutex=True, noinit=True)
+	# Using plib version of easygopigo3 to get noinit feature
+	egpg = easygopigo3.EasyGoPiGo3(use_mutex=True, noinit=True)
+	egpg.tp = tiltpan.TiltPan(egpg)
+	egpg.ds = egpg.init_distance_sensor("RPI_1")  # HW I2C
 
 	print("\n==== VOICE COMMANDER ====")
+
+	cmd_mode = True
+	verbose = True
 
 	while True:
 		# Wait for wakup keyword/phrase
 		try:
 			print("\nListening For Hotword (Marvin)")
-			carl_eyes(egpg,EYE_COLOR_HOTWORD)
+			eyes.carl_eyes(egpg,eyes.EYE_COLOR_HOTWORD)
 			detected = hotword.detectKeywords()
 
 		except KeyboardInterrupt:
-			carl_eyes(egpg,EYE_COLOR_OFF)
+			eyes.carl_eyes(egpg,eyes.EYE_COLOR_OFF)
 			break
 
 		if detected == "Exit":
-			carl_eyes(egpg,EYE_COLOR_OFF)
+			eyes.carl_eyes(egpg,eyes.EYE_COLOR_OFF)
 			break
 
-		# Listen For Command
+		# Listen For Command or Natural Language Phrase
 		try:
-			print("\nSimulating Listening For A Command")
-			carl_eyes(egpg,EYE_COLOR_COMMANDABLE)
-			time.sleep(3)
-			if random.choice([True, False]):
-				print("\nSimulating Command Accepted")
-				carl_eyes(egpg,EYE_COLOR_ACCEPTED)
-			else:
-				print("\nSimulating Command Rejected")
-				carl_eyes(egpg,EYE_COLOR_REJECTED)
-			time.sleep(.5)
+			if cmd_mode:
+				print("\nListening For A Command")
+				eyes.carl_eyes(egpg,eyes.EYE_COLOR_COMMANDABLE)
+				vcommand.reset_turn_start()
+				vcmd = vcommand.getVoiceCommand(timeout=10)
+				if vcommand.isExitRequest(vcmd):
+					eyes.carl_eyes(egpg,eyes.EYE_COLOR_REJECTED)
+					time.sleep(1)
+					break
+				elif vcmd == "TimeOut":
+					print("Command not heard before turn timeout")
+					eyes.carl_eyes(egpg,eyes.EYE_COLOR_REJECTED)
+					time.sleep(1)
+				elif vcmd == "natural language mode":
+					print("Entering Natural Language Mode")
+					cmd_mode = False
+					eyes.carl_eyes(egpg,eyes.EYE_COLOR_ACCEPTED)
+					time.sleep(1)
+				else:
+					eyes.carl_eyes(egpg,eyes.EYE_COLOR_ACCEPTED)
+					try:
+						vcommand.doVoiceAction(vcmd,egpg)
+						if vcmd == "be quiet":
+							verbose = False
+						elif vcmd == "you can talk now":
+							verbose = True
+					except KeyboardInterrupt:
+						break
+					except Exception as e:
+						print("Exception in vcommand.doVoiceCommand")
+						print(str(e))
+						traceback.print_stack()
+			else:  # nlu mode
+				while cmd_mode == False:
+					print("\nListening For Natural Language")
+					eyes.carl_eyes(egpg,eyes.EYE_COLOR_NL)
+					vcommand.reset_turn_start()
+					vphrase = vcommand.getVoiceNL(timeout=15)
+					if vcommand.isExitRequest(vphrase):
+						eyes.carl_eyes(egpg,eyes.EYE_COLOR_REJECTED)
+						time.sleep(1)
+						break
+					elif "command mode" in vphrase:
+						eyes.carl_eyes(egpg,eyes.EYE_COLOR_ACCEPTED)
+						alert = "Returning to COMMAND MODE"
+						print(alert)
+						cmd_mode = True
+						if verbose:
+							speak.say(alert)
+
+					elif vphrase == "TimeOut":
+						print("Nothing heard before turn timeout")
+						eyes.carl_eyes(egpg,eyes.EYE_COLOR_REJECTED)
+						time.sleep(1)
+					else:
+						eyes.carl_eyes(egpg,eyes.EYE_COLOR_ACCEPTED)
+						try:
+							print("Heard: ",vphrase)
+							if verbose:
+								speak.say("I heard ")
+								time.sleep(0.8)
+								speak.say(vphrase)
+							vcommand.doVoiceAction(vphrase,egpg)
+						except KeyboardInterrupt:
+							break
+						except Exception as e:
+							print("Exception in vcommand.doVoiceNLU")
+							print(str(e))
+							traceback.print_stack()
+
+
+
+
+
+
+
 		except KeyboardInterrupt:
-			carl_eyes(egpg,EYE_COLOR_OFF)
+			eyes.carl_eyes(egpg,eyes.EYE_COLOR_REJECTED)
+			time.sleep(1)
 			break
+
+	print("Exiting voicecmdr.py")
+	eyes.carl_eyes(egpg,eyes.EYE_COLOR_OFF)
 
 
 if __name__ == '__main__': main()
