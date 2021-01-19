8599a6b78d3b7dd504d9e30a63a0d6b7beacc20c
slowrunner <slowrunner@users.noreply.github.com>
2021-01-19 00:33:36 -0500

    move backup files to folder

--- backup.sh
+++ backup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-cp life.log life.log.backup
-cp wheel.log wheel.log.backup
-cp imu.log imu.log.backup
-cp run.log run.log.backup
-cp carlData.json carlData.json.backup
-cp juicer.out juicer.out.backup
+cp life.log backups/life.log.backup
+cp wheel.log backups/wheel.log.backup
+cp imu.log backups/imu.log.backup
+cp run.log backups/run.log.backup
+cp carlData.json backups/carlData.json.backup
+cp juicer.out backups/juicer.out.backup
 echo "backed up life, wheel, imu, run, juicer logs and carlData.json"
-ls -al *.backup
+ls -al backups/*.backup
 echo " "
 echo "Note: Does not backup google.cloud.cred.json"
+echo "      Does not backup openweathermap.key"
+echo "      Does not backup wunderground.key"
+echo "      Does not backup nyumaya_engine_carl/models"
+
