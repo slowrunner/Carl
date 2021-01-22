08e4c73a6aa25cef666f11be2a8059e6bde9656f
slowrunner <slowrunner@users.noreply.github.com>
2021-01-22 16:27:53 -0500

    releasing for full-time robot use

---
+++ Projects/VoiceCommander/diff_w_plib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+fn="voicecmdr.py"
+echo "diff local "$fn" with plib version"
+diff $fn ~/Carl/plib/$fn
+echo "No Difference If Nothing Listed"
---
+++ Projects/VoiceCommander/release_to_plib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fn="voicecmdr.py"
+echo "Copying local "$fn" to Carl/plib"
+cp $fn ~/Carl/plib/
+echo "Done"
+
