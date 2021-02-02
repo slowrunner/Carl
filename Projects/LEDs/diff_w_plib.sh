540fe63eb09f7884e5084fa343e4d9731a39ce06
slowrunner <slowrunner@users.noreply.github.com>
2021-02-02 00:00:08 -0500

    add wifi_blinker

---
+++ Projects/LEDs/diff_w_plib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+fn="leds.py"
+echo "diff local "$fn" with plib version"
+diff $fn ~/Carl/plib/$fn
+echo "No Difference If Nothing Listed"
---
+++ Projects/LEDs/release_to_plib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fn="leds.py"
+echo "Copying local "$fn" to Carl/plib"
+cp $fn ~/Carl/plib/
+echo "Done"
+
