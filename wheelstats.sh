e02cdf1271c6334457e6866a42ecf2b6a4287239
slowrunner <slowrunner@users.noreply.github.com>
2021-08-01 11:00:19 -0400

    fix awk sci notation errors into bc

--- wheelstats.sh
+++ wheelstats.sh
@@ -15,12 +15,12 @@ declare -i catchupSegmentss=1500
 totalTravel=`(awk -F'travel:' '{sum+=sqrt($2^2)}END{printf "%.1f", sum/1000;}' wheel.log)`
 totalTravelFt=`(echo "scale=1; ($totalTravel / 0.3048)" | bc)`
 echo "Total Travel: " $totalTravel "m" $totalTravelFt "ft"
-totalRotate=`(awk -F'rotation:' '{sum+=$2}END{print sum;}' wheel.log)`
+totalRotate=`(awk -F'rotation:' '{sum+=$2}END{printf "%.1f", sum;}' wheel.log)`
 totalRevs=`(echo "scale=1; $totalRotate / 360" | bc)`
 echo "Total Rotate: " $totalRotate "deg" $totalRevs "revolutions"
-totalMotion=`(awk -F'motion:' '{sum+=$2}END{print sum;}' wheel.log)`
+totalMotion=`(awk -F'motion:' '{sum+=$2}END{printf "%.1f", sum;}' wheel.log)`
 totalMotionHrs=`(echo "scale=3; $totalMotion / 3600" | bc)`
 echo "Total Motion: " $totalMotion "sec" $totalMotionHrs "hrs"
-totalLife=`(awk -F':' '{sum+=$3}END{print sum;}' life.log)`
+totalLife=`(awk -F':' '{sum+=$3}END{printf "%.1f", sum;}' life.log)`
 percentInMotion=`(echo "scale=2; $totalMotionHrs * 100.0 / $totalLife" | bc)`
 echo "Total Life: " $totalLife "hrs   percentInMotion:" $percentInMotion
