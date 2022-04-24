172395ba0ea7714d783290b3b51c6643fd07ce78
slowrunner <slowrunner@users.noreply.github.com>
2022-04-24 14:53:45 -0400

    more features

--- utils/gopigo_sw_version.sh
+++ utils/gopigo_sw_version.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Dexter easygopigo3.py sources version:"
+grep "__version__"  ~/Dexter/GoPiGo3/Software/Python/easygopigo3.py
+
+echo "Installed gopigo3 Python egg files"
+find /usr/local/lib/python* -name *.egg | grep gopigo3
+find /usr/local/lib/python* -name *.egg | grep DI_
+find /usr/local/lib/python* -name *.egg | grep Dexter
 
 echo "GoPiGo3 Python3 Software Version"
 pip3 freeze | grep gopigo
