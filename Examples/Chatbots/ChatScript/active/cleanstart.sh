0f103edc2e635c57e476862c1bb4b2a1e0672d94
slowrunner <slowrunner@users.noreply.github.com>
2022-04-26 13:29:39 -0400

    initial

---
+++ Examples/Chatbots/ChatScript/active/cleanstart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Restore ChatScript to no history
+# echo "Removing files from TMP/ USERS/ LOGS/"
+echo "Removing files from USERS/"
+# rm TMP/*
+rm USERS/*
+# rm LOGS/*
+echo "Done"
