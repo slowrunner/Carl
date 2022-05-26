eebeba7af756b95b22332fb71c43705661e3f67b
slowrunner <slowrunner@users.noreply.github.com>
2022-05-26 12:33:04 -0400

    create folder if needed

--- systests/camera/takeVideo.py
+++ systests/camera/takeVideo.py
@@ -7,6 +7,8 @@
 from picamera import PiCamera
 from time import sleep
 from datetime import datetime
+import os
+
 
 VIDEO_LENGTH = 10 # seconds
 HRES = 640
@@ -16,8 +18,13 @@ WAIT_TIME = 5
 camera = PiCamera()
 camera.resolution = (HRES, VRES)
 
+if not os.path.exists('videos'):
+    os.makedirs('videos')
+
 print("Waiting {}s for camera to settle".format(WAIT_TIME))
 sleep(WAIT_TIME) # 0.25 good light, 5.0 when dark - allow picam to adjust exposure
+
+
 fname = "videos/video_"+datetime.now().strftime("%Y%m%d-%H%M%S")+".h264"
 fn = camera.start_recording(fname)
 camera.wait_recording(VIDEO_LENGTH)
