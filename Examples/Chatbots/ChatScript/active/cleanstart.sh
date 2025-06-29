a76b72c3f91104c52b4cdef8407b745fabfb628b
slowrunner <slowrunner@users.noreply.github.com>
2025-06-28 22:39:15 -0400

    deleted ChatScript bot

--- Examples/Chatbots/ChatScript/active/cleanstart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Restore ChatScript to no history
-# echo "Removing files from TMP/ USERS/ LOGS/"
-echo "Removing files from USERS/"
-# rm TMP/*
-rm USERS/*
-# rm LOGS/*
-echo "Done"
