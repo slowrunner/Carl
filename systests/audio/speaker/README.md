6a3b394d73d1e8cd18769a1d2483b22db069ef64
slowrunner <slowrunner@users.noreply.github.com>
2025-10-04 10:45:05 -0400

    guidance when no audio out (after update/upgrade)

---
+++ systests/audio/speaker/README.md
@@ -0,0 +1,6 @@
+# Speaker For Carl
+
+- Test Speaker Output
+  - espeak-ng hello
+  - if no output: sudo raspi-config, system options, audio->headphones
+  - run speakerTest.sh
--- systests/audio/speaker/speakerTest.sh
+++ systests/audio/speaker/speakerTest.sh
@@ -3,4 +3,4 @@
 speaker-test -t wav -c 2 -l 1
 sleep 5
 aplay /usr/share/scratch/Media/Sounds/Animal/Duck.wav
-
+espeak-ng "If you are hearing this, audio output is correctly set up"
